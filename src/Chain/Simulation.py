a8b3916f98f16f71f606128615299559b705eb3d
George Diamantopoulos <Giorg.diama@gmail.com>
2023-10-10 17:31:20 +0100

    unused imports + comments

--- src/Chain/Simulation.py
+++ src/Chain/Simulation.py
@@ -5,24 +5,18 @@ from Chain.Parameters import Parameters
 from Chain.EventQueue import Queue
 from Chain.Handler import handle_event
 
-from Chain.Consensus.PBFT.PBFT import PBFT
-from Chain.Consensus.BigFoot.BigFoot import BigFoot
-
 import Chain.tools as tools
 
 from Chain.Event import SystemEvent
-
-import sys
 
 
 class Simulation:
     def __init__(self) -> None:
         self.q = Queue()
+        self.clock = 0
 
         self.nodes = [Node(x, self.q)
                       for x in range(Parameters.application["Nn"])]
-
-        self.clock = 0
 
         self.current_cp = Parameters.application['CP']
 
@@ -33,25 +27,26 @@ class Simulation:
     def init_simulation(self):
         genesis = Block.genesis_block()
 
+        # GENERATE THE INITIAL SET OF TRANSACTIONS (the manager takes over after the intial generation)
         Parameters.simulation['txion_model'].generate_interval_txions(
             self.clock)
 
+        # for each node: add the genesis block, set and initialise CP (NOTE: cp.init() produces the first events to kickstart the simulation)
         for n in self.nodes:
             n.add_block(genesis, self.clock)
             n.cp = self.current_cp(n)
             n.cp.init()
 
     def sim_next_event(self):
+        # get next event
         next_event = self.q.pop_next_event()
-
+        # update sim clocks
         self.clock = next_event.time
-
-        print(
-            f"\rSimulation clock: {'%.2f'%self.clock}/{Parameters.simulation['simTime']}", end='', flush=True)
 
         tools.debug_logs(msg=tools.sim_info(self, print_event_queues=True),
                          command=f"Next event: {next_event}", simulator=self)
 
+        # call appropirate handler based on event type
         if isinstance(next_event, SystemEvent):
             self.manager.handle_next_event(next_event)
         else:
