c8488c7108c1d803283b5c8900a129e48fcafb92
George Diamantopoulos <Giorg.diama@gmail.com>
2023-10-18 16:26:19 +0100

    added logic to track event submissions per node

--- src/Chain/EventQueue.py
+++ src/Chain/EventQueue.py
@@ -1,6 +1,24 @@
 from Chain.Parameters import Parameters
-
+from Chain.Event import SystemEvent
 import heapq
+
+
+def log_events(event):
+    if isinstance(event, SystemEvent):
+        if event.payload["type"] in Parameters.simulation["events"].keys():
+            Parameters.simulation["events"][event.payload["type"]] += 1
+        else:
+            Parameters.simulation["events"][event.payload["type"]] = 1
+    else:
+        if event.payload["type"] not in Parameters.simulation["events"].keys():
+            Parameters.simulation["events"][event.payload["type"]] = {}
+
+        event_type = Parameters.simulation["events"][event.payload["type"]]
+
+        if event.actor.id in event_type:
+            event_type[event.actor.id] += 1
+        else:
+            event_type[event.actor.id] = 1
 
 
 class PrioQueue:
@@ -12,11 +30,7 @@ class PrioQueue:
         self.pq = []
 
     def add_task(self, task, priority):
-        if task.payload["type"] in Parameters.simulation["events"].keys():
-            Parameters.simulation["events"][task.payload["type"]] += 1
-        else:
-            Parameters.simulation["events"][task.payload["type"]] = 1
-
+        log_events(task)
         heapq.heappush(self.pq, (priority, task))
 
     def pop_task(self):
