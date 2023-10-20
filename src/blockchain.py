5404bb0bfedcb640ee9cd99c0eee5e143bf67070
George Diamantopoulos <Giorg.diama@gmail.com>
2023-10-20 12:58:54 +0100

    Update blockchain.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -5,30 +5,22 @@ from Chain.Manager.Manager import Manager
 import random
 import numpy
 
-from Chain.Consensus.PBFT.PBFT_state import PBFT
-from Chain.Consensus.BigFoot.BigFoot_state import BigFoot
-
 from Chain.Metrics import Metrics
 import Chain.tools as tools
+
+import statistics as st
 
 ############### SEEDS ############
 seed = 5
 random.seed(seed)
 numpy.random.seed(seed)
-############### SEEDS ############
-
-
-CPs = {
-    PBFT.NAME: PBFT,
-    BigFoot.NAME: BigFoot
-}
+############## SEEDS ############
 
 
 def run():
     manager = Manager()
 
     manager.set_up()
-
     t = datetime.now()
     manager.run()
     runtime = datetime.now() - t
@@ -36,40 +28,23 @@ def run():
     print(tools.color(
         f"Simulated time {'%.2f'%manager.sim.clock} seconds!", 45))
 
-    # Metrics.save_snapshots("snapshot")
     Metrics.measure_all(manager.sim)
     Metrics.print_metrics()
 
     s = f"{'-'*30} EVENTS {'-'*30}"
     print(tools.color(s, 41))
-    print(Parameters.simulation['events'])
+
+    for key, value in Parameters.simulation['events'].items():
+        if isinstance(value, dict):
+            print(key)
+
+            # gourp by value
+            s = ' '.join(f'{node}:{num}' for node, num in value.items())
+            print(s)
+        else:
+            print(key, value)
 
     print(tools.color(f"SIMULATION EXECUTION TIME: {runtime}", 45))
 
 
-# def test_plot():
-#     import json
-#     import matplotlib.pyplot as plt
-
-#     with open(f"results/snapshot.json", "r") as f:
-#         data = json.load(f)
-
-#     times = []
-#     throughputs = []
-
-#     for key, value in data.items():
-#         times.append(value['time'])
-#         throughputs.append(value["metrics"]['throughput']['0'])
-#         bws = [value['nodes'][key]['bandwidth'] for key in value['nodes']]
-
-#     import matplotlib.pyplot as plt
-#     import matplotlib as mpl
-
-#     mpl.use("pgf")
-
-#     plt.plot(range(10))
-#     plt.savefig("test.pdf")
-
-
 run()
-# test_plot()
