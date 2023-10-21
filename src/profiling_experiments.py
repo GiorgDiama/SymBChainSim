b30211befa9ee4cf3ad919507a9e8aa9aa5bbcb3
George Diamantopoulos <Giorg.diama@gmail.com>
2023-10-21 18:17:02 +0100

    added parameter control in functions

--- src/profiling_experiments.py
+++ src/profiling_experiments.py
@@ -12,14 +12,14 @@ import Chain.tools as tools
 
 import statistics as st
 
-# ############### SEEDS ############
-# seed = 5
-# random.seed(seed)
-# numpy.random.seed(seed)
-# ############## SEEDS ############
+############### SEEDS ############
+seed = 5
+random.seed(seed)
+numpy.random.seed(seed)
+############## SEEDS ############
 
 
-def profile_memeory(num_nodes, num_repeats):
+def profile_memeory(num_nodes, num_repeats, cp, mempool):
     manager = Manager()
 
     mem = []
@@ -27,6 +27,10 @@ def profile_memeory(num_nodes, num_repeats):
         tracemalloc.start()
 
         manager.load_params()
+        Parameters.application['Nn'] = num_nodes
+        Parameters.simulation['init_CP'] = cp
+        Parameters.application['transaction_model'] = mempool
+
         manager.set_up(num_nodes)
         manager.run()
 
@@ -36,13 +40,16 @@ def profile_memeory(num_nodes, num_repeats):
     return st.mean(mem)
 
 
-def profile_runtime(num_nodes, num_repeats):
+def profile_runtime(num_nodes, num_repeats, cp, mempool):
     manager = Manager()
 
     runtimes = []
     simulated_clock = []
     for _ in range(num_repeats):
         manager.load_params()
+        Parameters.application['Nn'] = num_nodes
+        Parameters.simulation['init_CP'] = cp
+        Parameters.application['transaction_model'] = mempool
         manager.set_up(num_nodes)
         t = datetime.now()
         manager.run()
@@ -53,11 +60,33 @@ def profile_runtime(num_nodes, num_repeats):
     return st.mean(runtimes), st.mean(simulated_clock)
 
 
-def run_profile_experiment(name=''):
+def profile_events(num_nodes, cp, mempool):
+    # events produced are not affected by system processes so no need to repeat
+    manager = Manager()
+
+    events = {}
+    manager.load_params()
+    Parameters.application['Nn'] = num_nodes
+    Parameters.simulation['init_CP'] = cp
+    Parameters.application['transaction_model'] = mempool
+    manager.set_up()
+    manager.run()
+
+    for key, value in Parameters.simulation['events'].items():
+        if isinstance(value, int):
+            events[key] = value
+        else:
+            events[key] = sum([v for v in value.values()])
+
+    return events
+
+
+def run_profile_experiment(name, cp, mempool):
     data = {}
     for num_nodes in range(5, 105, 5):
-        wall_clock, simulation_time = profile_runtime(num_nodes, 3)
-        peak_memory = profile_memeory(num_nodes, 1)
+        wall_clock, simulation_time = profile_runtime(
+            num_nodes, 3, cp, mempool)
+        peak_memory = profile_memeory(num_nodes, 3, cp, mempool)
         print(f'{num_nodes}\t{wall_clock}\t{peak_memory}')
         data[num_nodes] = {
             'wall_clock': wall_clock,
@@ -69,4 +98,26 @@ def run_profile_experiment(name=''):
         json.dump(data, f, indent=4)
 
 
-run_profile_experiment(name='parametersA_local_with_prop')
+def run_profile_events_experiment(name=''):
+    data = {}
+    for num_nodes in range(5, 105, 5):
+        events = profile_events(num_nodes)
+
+        print(f'{num_nodes}\t{events}')
+        data[num_nodes] = {
+            'events': events
+        }
+
+    with open(f"Results/profiling_events_{name}.json", 'w+') as f:
+        json.dump(data, f, indent=4)
+
+
+run_profile_experiment(name='final_parametersA_global(3avg_PBFT)',
+                       cp='PBFT',
+                       mempool='global')
+
+run_profile_experiment(name='final_parametersA_local(3avg_PBFT)',
+                       cp='PBFT',
+                       mempool='local')
+
+# run_profile_events_experiment(name='parametersA(BigFoot)')
