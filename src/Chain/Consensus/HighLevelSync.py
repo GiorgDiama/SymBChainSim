ccce31dca7e439902156faef545e5c3d0f76c3ae
George Diamantopoulos <Giorg.diama@gmail.com>
2024-11-03 12:27:16 +0800

    Nodes now call add_block to add received blocks (ensuring transactions are removed from their local pool)

--- src/Chain/Consensus/HighLevelSync.py
+++ src/Chain/Consensus/HighLevelSync.py
@@ -3,13 +3,10 @@
     (missing blocks) and creates a local event which copies the missing blocks to the de-synced node saving communication
     events
 '''
+from ..Network import Network
+from ..Parameters import Parameters
 
-from Chain.Network import Network
-from Chain.Parameters import Parameters
-
-import Chain.tools as tools
-
-from random import randint, sample
+from random import sample
 
 
 def handler(event):
@@ -17,7 +14,6 @@ def handler(event):
         return handle_local_sync_event(event)
     else:
         return "unhandled"
-
 
 def create_local_sync_event(desynced_node, request_node, time):
     '''
@@ -35,10 +31,12 @@ def create_local_sync_event(desynced_node, request_node, time):
     missing_blocks = []
     for b in reversed(request_node.blockchain):
         if b.depth > latest_block.depth:
+            # TODO: Insert is o(n) ... append o(1) and reverse 
             missing_blocks.insert(0, b.copy())
         else:
             break
-
+        
+    # take into account a static delay for sending the sync message
     total_delay = Parameters.execution['sync_message_request_delay']
 
     # for each missing block
@@ -54,27 +52,11 @@ def create_local_sync_event(desynced_node, request_node, time):
         # add the delay of the current block to the total delay
         total_delay += delay
 
-        # update time desynced node will receive missing block
+        # update the time added for the block and mark it as a 'synced' block
         missing_blocks[i].time_added = time + total_delay
         missing_blocks[i].extra_data['synced'] = True
 
-    # misbehave_delay, misbehaviour = apply_sync_misbehaviour(request_node)
-
-    # # create local sync event on desynced_node
-    # if missbehaviour:  # the request node missbehaved
-
-    #     payload = {
-    #         "request_node": request_node,
-    #         "type": 'local_fast_sync',
-    #         "blocks": None,
-    #         "fail": True,
-    #     }
-
-    #     desynced_node.scheduler.schedule_event(
-    #         desynced_node, time+missbehave_delay, payload, handler)
-    # else:
-    # add an event signifying the end of the transmission of missing blocks
-
+    # schedule the sync event
     payload = {
         "request_node": request_node,
         "type": 'local_fast_sync',
@@ -88,10 +70,10 @@ def create_local_sync_event(desynced_node, request_node, time):
 
 def handle_local_sync_event(event):
     '''
-        Checks for failures and copies missing blocks to end of the callers blockchain. 
-        Checks if the node we are syncing with has received any blocks while we were waiting for the currents once
-            If so, tried to get the new blocks 
-        Relies on calling the CP specific resync method for the node to rejoin the consensus process 
+        Copies missing blocks to end of the callers blockchain. 
+        Additionally, checks if the node we are syncing with has received any blocks while we were waiting for the currents once
+            If so, also requests the new blocks through another local_sync_event
+        Relies on calling the CP specific rejoin method for the node to rejoin the consensus process 
     '''
     node = event.creator
 
@@ -99,64 +81,29 @@ def handle_local_sync_event(event):
         # if the previous request failed - try to sync with a random neighbour
         create_local_sync_event(node, sample(
             node.neighbours, 1)[0], event.time)
-    else:  # successfully synced
-        received_blocks = event.payload['blocks']
-        # update blockchain with received blocks
-        for b in received_blocks:
-            # there is a chance the node was updated before this message made it to them
-            # so checking to not add repeat blocks
-            if b.depth == node.blockchain[-1].depth + 1:
-                node.blockchain.append(b)
+        return 'sync_failed'
+    
+    # successfully synced
+    received_blocks = event.payload['blocks']
+    # update blockchain with received blocks
+    for b in received_blocks:
+        # there is a chance the node was updated before this message made it to them
+        # checking depth to prevent adding repeat blocks
+        if b.depth == node.blockchain[-1].depth + 1:
+            node.add_block(b, -1, update_time_added=True)
 
-        # check if the request node has received any new blocks while we were syncing
-        if node.last_block.depth < event.payload["request_node"].last_block.depth:
-            # if so continue the sync process
-            create_local_sync_event(
-                node, event.payload["request_node"], event.time)
-            return 0
+    # check if the request node has received any new blocks while we were syncing
+    if node.last_block.depth < event.payload["request_node"].last_block.depth:
+        # if so continue the sync process
+        create_local_sync_event(
+            node, event.payload["request_node"], event.time)
+        return 0
 
-        # adds time of final check
-        event.time += Parameters.execution["sync_message_request_delay"]
+    # adds time of final check
+    event.time += Parameters.execution["sync_message_request_delay"]
 
-        # change state to sync
-        node.state.synced = True
+    # change state to sync
+    node.state.synced = True
+    node.cp.rejoin(event.time)
 
-        # perform the CP specific resync actions
-        if received_blocks:
-            node.cp.resync(event.payload, event.time)
-
-        if node.update(event.time):
-            return True
-
-
-# def apply_sync_missbehaiviour(sender):
-#     '''
-#         Checks whether the requesting node is:
-#             - offline: adds no_response delay
-#             - byzantine: randomly drops message (no_response delay) or reply with bad data (bad_data delay)
-#     '''
-#     # if request_node is dead, sync fails with no delay
-#     if not sender.state.alive:
-#         return Parameters.behaiviour["sync"]["no_response"]["delay"], True
-
-#     # if the request node is byzantine
-#     if sender.behaviour.byzantine:
-#         # roll for missbehave
-#         roll_missbehave = randint(0, 100)
-#         if roll_missbehave < sender.behaviour.sync_fault_chance:
-#             # if missbehave: roll for type
-#             roll_type = randint(0, 100)
-#             if roll_type < 50:
-#                 ########### BAD DATA ############
-#                 tools.debug_logs(
-#                     msg=f"node {sender} sent bad sync data!", col=47)
-#                 delay = Parameters.behaiviour["sync"]["bad_data"]["delay"]
-#             else:
-#                 ########### NO RESPONSE #########
-#                 tools.debug_logs(
-#                     msg=f"node {sender} did not respond to sync message!", col=47)
-#                 delay = Parameters.behaiviour["sync"]["no_response"]["delay"]
-#             return delay, True
-
-#     # node will not missbehaive
-#     return 0, False
+    return 'successfully_synced'
